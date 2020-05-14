89cbe98338fafc86558c6940b362ab104cf6a7fb
Isabela Araujo <belaaraujobr@protonmail.com>
2020-05-14 18:16:15 -0300

    contar paragrafos e retirar linhas em branco dos arquivos

--- text_treatment.py
+++ text_treatment.py
@@ -1,16 +1,41 @@
-#script para ler txt e contar palavras
 import fileinput
+import os
 
-file=open('diabetes_e_saude_bucal.txt', 'r')
+path ="F:/Faculdade/UFG/Mestrado/NPL/AmericansHealth/arquivos_diabetes"
 
-#contar paragrafos e tirar linhas em branco
-with file as f:
-#    texto = [texto.rstrip() for texto in f]
-    texto = list(filter(lambda value: value != '', [value.replace('\n', '').strip('"') for value in f])) #tira \n
+filelist = [] #lista de arquivos da pasta
 
-qnt_paragrafos= (len(texto))-1 #nao leva em consideracao o titulo do arquivo
-print(texto[0])
-print("Quantidade de paragrafos: ", qnt_paragrafos)
-print("Quantidade de paragrafos + titulo: ", len(texto))
-print(texto)
+#arquivo de estatisticas
+arquivo = open('estatistica.txt', 'w')
+arquivo.write('Arquivo'+'\t')
+arquivo.write('qnt_paragrafos'+'\t')
+arquivo.write('qnt_palavras' +'\n')
+arquivo.close()
 
+#lista todos arquivos de uma pasta
+for root, dirs, files in os.walk(path):
+	for file in files:
+		if(file.endswith(".txt")):
+			filelist.append(file)
+			#print(os.path.join(root,file))
+
+for aux in filelist:
+
+	#abre arquivo selecionado
+	f = open(aux, 'r')
+	#paragrafos
+	paragrafos = list(filter(lambda value: value != '', [value.replace('\n', '').strip('"') for value in f]))
+
+	f = open(aux, 'r')
+	data = f.read() #todo texto
+	words = data.split() #palavras
+	qnt_parag = str(len(paragrafos)-1)
+	qnt_words=str(len(words))
+	#print(paragrafos)
+	#print(words)
+
+	arquivo = open('estatistica.txt', 'a')
+	arquivo.write(aux + '\n')
+	arquivo.write(qnt_parag + '\t')
+	arquivo.write(qnt_words + '\n')
+	arquivo.close()
