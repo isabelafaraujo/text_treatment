43b15ed53d86a61092c917218a17be170cb5f5cd
Isabela Araujo <belaaraujobr@protonmail.com>
2020-05-15 01:20:55 -0300

    contar paragrafos e retirar linhas em branco dos arquivos

---
+++ find_questions.py
@@ -0,0 +1,20 @@
+import os
+
+path = "F:/Faculdade/UFG/Mestrado/NPL/AmericansHealth/arquivos_diabetes"
+
+filelist = []
+
+for root, dirs, files in os.walk(path):
+    for file in files:
+        if file.endswith(".txt"):
+            filelist.append(file)
+
+for arquivo in filelist:
+    f = open(arquivo, "r", encoding='ISO-8859-1')
+    frases = f.readlines()
+    words = '?'
+
+    for frase in frases:
+        for pergunta in frase:
+            if pergunta == '?':
+                print(frase + arquivo + '\n')
